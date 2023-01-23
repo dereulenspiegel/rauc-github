00d075a0e6ba8043403730ef6bfcd9aaf5a6dcc9
Till Klocke <till.klocke@gmail.com>
2023-01-23 20:32:08 +0100

    Added test for successful async installation

--- manager_test.go
+++ manager_test.go
@@ -2,7 +2,9 @@ package raucgithub
 
 import (
 	"context"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/dereulenspiegel/raucgithub/mocks"
@@ -191,4 +193,79 @@ func TestCheckUpdateWithoutAssetName(t *testing.T) {
 	assert.Equal(t, "Penguin", update.Name)
 	assert.Equal(t, "https://example.com/cbpifw-raspberrypi3-64_v1.8.2.bundle", bundle.URL)
 	assert.Equal(t, "cbpifw-raspberrypi3-64_v1.8.2.bundle", bundle.AssetName)
+}
+
+func TestRunUpdate(t *testing.T) {
+	repo := mocks.NewRepository(t)
+	raucClient := mocks.NewRaucDBUSClient(t)
+	t.Cleanup(func() {
+		repo.AssertExpectations(t)
+		raucClient.AssertExpectations(t)
+	})
+
+	currentVersion, err := semver.NewVersion("1.8.1")
+	require.NoError(t, err)
+
+	updater, err := NewUpdateManager(repo, WithRaucClient(raucClient))
+	require.NoError(t, err)
+
+	repo.EXPECT().Updates(mock.Anything).Return([]repository.Update{
+		{
+			Version: semver.New("1.2.2"),
+		},
+		{
+			Version: semver.New("1.6.6"),
+		},
+		{
+			Name:    "Penguin",
+			Version: semver.New("1.8.2"),
+			Bundles: []repository.BundleLink{
+				{
+					URL: "https://example.com/cbpifw-raspberrypi3-64_v1.8.2.bundle",
+				},
+			},
+		},
+	}, nil)
+	raucClient.EXPECT().GetBootSlot().Return("slot0", nil)
+	raucClient.EXPECT().GetCompatible().Return("cbpifw-raspberrypi3-64", nil)
+	raucClient.EXPECT().GetSlotStatus().Return([]rauc.SlotStatus{
+		{
+			SlotName: "slot0",
+			Status: map[string]dbus.Variant{
+				"bundle.version": dbus.MakeVariant(currentVersion.String()),
+				"bundle.foo":     dbus.MakeVariant("bar"),
+			},
+		},
+		{
+			SlotName: "slot1",
+			Status: map[string]dbus.Variant{
+				"bundle.version": dbus.MakeVariant("1.7.0"),
+				"bundle.foo":     dbus.MakeVariant("bar"),
+			},
+		},
+	}, nil)
+
+	update, bundle, err := updater.CheckForUpdate(context.Background())
+	require.NoError(t, err)
+	assert.NotNil(t, update)
+	assert.NotNil(t, bundle)
+
+	assert.Equal(t, "Penguin", update.Name)
+	assert.Equal(t, "https://example.com/cbpifw-raspberrypi3-64_v1.8.2.bundle", bundle.URL)
+	assert.Equal(t, "cbpifw-raspberrypi3-64_v1.8.2.bundle", bundle.AssetName)
+
+	raucClient.EXPECT().InstallBundle("https://example.com/cbpifw-raspberrypi3-64_v1.8.2.bundle", mock.Anything).After(time.Millisecond * 100).Return(nil)
+
+	raucClient.EXPECT().GetProgress().Return(75, "Installing", 1, nil)
+	raucClient.EXPECT().GetOperation().Maybe().Return("installing")
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	updateChan := updater.InstallUpdateAsync(context.Background(), func(success bool, err error) {
+		assert.True(t, success)
+		assert.NoError(t, err)
+		wg.Done()
+	})
+	wg.Wait()
+	assert.GreaterOrEqual(t, len(updateChan), 1)
 }
