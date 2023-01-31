7864a4ee6d81a381f5e7076326bbbc4a49f6a47d
Till Klocke <till.klocke@gmail.com>
2023-01-31 14:35:28 +0100

    We should be able to check for updates and status now, but needs testing

--- server/unix/unix.go
+++ server/unix/unix.go
@@ -2,6 +2,7 @@ package unix
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net"
@@ -10,7 +11,8 @@ import (
 
 	logrusLogger "github.com/chi-middleware/logrus-logger"
 	"github.com/dereulenspiegel/raucgithub"
-	"github.com/go-chi/chi"
+	"github.com/dereulenspiegel/raucgithub/repository"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -53,7 +55,13 @@ func (s *SocketServer) Start(ctx context.Context) error {
 	r.Use(middleware.CleanPath)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.AllowContentType("application/json"))
 	r.Use(logrusLogger.Logger("router", s.logger))
+
+	r.Route("/update", func(r chi.Router) {
+		r.Get("/check", s.checkUpdate)
+		r.Get("/status", s.status)
+	})
 
 	unixListener, err := net.Listen("unix", s.socketPath)
 	if err != nil {
@@ -74,5 +82,99 @@ func (s *SocketServer) Close() error {
 func (s *SocketServer) listen(l net.Listener) {
 	if err := s.server.Serve(l); err != http.ErrServerClosed {
 		s.logger.WithError(err).Error("failed to serve on unix socket")
+	}
+}
+
+type updateResponse struct {
+	Version     string    `json:"version"`
+	ReleaseDate time.Time `json:"releaseDate"`
+	Name        string    `json:"name"`
+	Notes       string    `json:"notes"`
+	Prerelease  bool      `json:"prerelease"`
+}
+
+func responseFromUpdate(update *repository.Update) updateResponse {
+	return updateResponse{
+		Version:     update.Version.String(),
+		ReleaseDate: update.ReleaseDate,
+		Name:        update.Name,
+		Notes:       update.Notes,
+		Prerelease:  update.Prerelease,
+	}
+}
+
+func (s *SocketServer) checkUpdate(w http.ResponseWriter, r *http.Request) {
+	status, err := s.manager.Status(r.Context())
+	if err != nil {
+		panic(err)
+	}
+	if status != raucgithub.StatusIdle {
+		writeError(errors.New("update service is not idle"), http.StatusConflict, w)
+		return
+	}
+
+	update, err := s.manager.CheckForUpdate(r.Context())
+	if err == raucgithub.ErrNoSuitableUpdate {
+		writeError(errors.New("no update"), http.StatusNotFound, w)
+		http.Error(w, "no update", http.StatusNotFound)
+		return
+	} else if err != nil {
+		panic(err)
+	}
+
+	resp := responseFromUpdate(update)
+
+	writeResponse(resp, http.StatusOK, w)
+}
+
+type statusResponse struct {
+	Status     string          `json:"status"`
+	Progress   *int32          `json:"progress,omitempty"`
+	NextUpdate *updateResponse `json:"nextUpdate,omitempty"`
+}
+
+func (s *SocketServer) status(w http.ResponseWriter, r *http.Request) {
+	status, err := s.manager.Status(r.Context())
+	if err != nil {
+		panic(err)
+	}
+	resp := statusResponse{
+		Status: status.String(),
+	}
+	if status == raucgithub.StatusInstalling {
+		progress, err := s.manager.Progress(r.Context())
+		if err != nil {
+			panic(err)
+		}
+		resp.Progress = &progress
+	} else {
+		update, err := s.manager.CheckForUpdate(r.Context())
+		if err == nil {
+			updateResp := responseFromUpdate(update)
+			resp.NextUpdate = &updateResp
+		} else if err != nil && err != raucgithub.ErrNoSuitableUpdate {
+			panic(err)
+		}
+	}
+
+	writeResponse(resp, http.StatusOK, w)
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+func writeError[T error](err T, status int, w http.ResponseWriter) {
+	errRsp := errorResponse{
+		Error: err.Error(),
+	}
+	writeResponse(errRsp, status, w)
+}
+
+func writeResponse[T any](resp T, status int, w http.ResponseWriter) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		panic(err)
 	}
 }
